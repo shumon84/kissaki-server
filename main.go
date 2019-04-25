6d14205afc67ce99751dbcab79878a2c9edbb82b
shumon84 <cameremon84@gmail.com>
2019-04-26 00:10:13 +0900

    Getリクエストを処理するハンドラを追加

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"time"
 )
 
@@ -14,4 +16,13 @@ type StageInfo struct {
 	StartPosition uint8     `json:"start_position"`
 	GoalPosition  uint8     `json:"goal_position"`
 	CreatedAt     time.Time `json:"created_at"`
+}
+
+var (
+	MaxNumOfStages = 30
+	DB             = make([]StageInfo, 0)
+)
+
+func GetHandler(w http.ResponseWriter, _ *http.Request) {
+	json.NewEncoder(w).Encode(DB)
 }
